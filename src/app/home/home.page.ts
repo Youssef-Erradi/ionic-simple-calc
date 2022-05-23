b782a7131c956d3e9511cc605fe11c4540e8b272
Youssef Erradi <74113158+Youssef-Erradi@users.noreply.github.com>
2022-05-23 10:44:01 +0100

    add typescript code

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,13 @@ export class HomePage {
 
   constructor() {}
 
+  calc(operation:string){
+    let nb1 = document.getElementById('nb1') as HTMLInputElement;
+    let nb2 = document.getElementById('nb2')as HTMLInputElement;
+    let result = document.getElementById('result');
+    let output =  eval(`${nb1.value}${operation}${nb2.value}`)+"";
+    if(output === "NaN" || output === "Infinity")
+      output = "la division par 0 est impossible";
+    result.innerText = output;
+  }
 }
